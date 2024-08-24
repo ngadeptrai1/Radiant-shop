1e135151610eebeb5f67aa3302bf0112d78419d3
DucDuy <duybung2004@gmail.com>
2024-08-24 21:50:56 +0700

    Home page design

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'my-angular-app';
-}
+export class AppComponent {}
